c6a9f4ff064ab8fb26c07849a4219791de31de78
boynuibon3s <boynuibon3s@gmail.com>
2020-10-09 12:38:23 +0700

    game_itit bug??

--- Game_Window/Load_sprite/WinMain3.cpp
+++ Game_Window/Load_sprite/WinMain3.cpp
@@ -1,65 +1,38 @@
-﻿#include <windows.h>
-//cac file header cân đưa vao
+﻿// winmain.cpp - Windows framework source code file
 #include <d3d9.h>
 #include <d3dx9.h>
 #include <time.h>
-
-
-//tiêu đê ưng dung
-#define APPTITLE "Load_Bitmap"
-
-//cac macro đê đọc phim
-#define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000)? 1 : 0)
-#define KEY_UP(vk_code)((GetAsyncKeyState(vk_code) & 0x8000) ? 1: 0)
-
-	//đô phân giai man hinh
-#define SCREEN_WIDTH 520
-#define SCREEN_HEIGHT 720
-
-//cac khai bao ham
-LRESULT WINAPI WinProc(HWND, UINT, WPARAM, LPARAM);
-
-ATOM MyRegisterClass(HINSTANCE);
-int Game_Init(HWND);
-void Game_Run(HWND);
-void Game_End(HWND);
-
-HWND hWnd = 0;
-
-LPDIRECT3D9 d3d = NULL;
-LPDIRECT3DDEVICE9 d3ddev = NULL;
-LPDIRECT3DSURFACE9 backbuffer = NULL;
-int BackBufferWidth = 0;
-int BackBufferHeight = 0;
-LPDIRECT3DSURFACE9 surface = NULL;
-
-
-
-//-----------------------------------------------------------
-
+#include <stdio.h>
+#include "dxgraphics.h"
+#include "game.h"
 //window event callback function
 LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
 	case WM_DESTROY:
+		//release the Direct3D device
+		if (d3ddev != NULL)
+			d3ddev->Release();
+		//release the Direct3D object
+		if (d3d != NULL)
+			d3d->Release();
+		//call the "front-end" shutdown function
 		Game_End(hWnd);
+		//tell Windows to kill this program
 		PostQuitMessage(0);
 		return 0;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
-
-//-------------------------------------------------------------------------
-
-//cac ham hô trợ đê khơi đông Window
-ATOM MyRegisterClass(HINSTANCE hInstance) //creatGameWindow
+//helper function to set up the window properties
+ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	//create the window class structure
 	WNDCLASSEX wc;
 	wc.cbSize = sizeof(WNDCLASSEX);
 
-	//điên tham sô ham vao struct
+	//fill the struct with info
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = (WNDPROC)WinProc;
 	wc.cbClsExtra = 0;
@@ -67,171 +40,31 @@ ATOM MyRegisterClass(HINSTANCE hInstance) //creatGameWindow
 	wc.hInstance = hInstance;
 	wc.hIcon = NULL;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wc.lpszMenuName = NULL;
 	wc.lpszClassName = APPTITLE;
 	wc.hIconSm = NULL;
-	//đăng ky lơp cưa sô
+	//set up the window with the class info
 	return RegisterClassEx(&wc);
-
-
 }
-
-//-----------------------------------------------------------------------
-
-int Game_Init(HWND hwnd)
-{
-	HRESULT result;
-	//Đối tượng Direct3D được tao bằng một lệnh đơn gian sau
-	//initialize Direct3D
-	d3d = Direct3DCreate9(D3D_SDK_VERSION);
-
-	if (d3d == NULL)
-	{
-		MessageBox(hwnd, "Error initializing Direct3D", "Error",
-			MB_OK);
-		return 0;
-	}
-
-	//set Direct3D presentation parameters
-	D3DPRESENT_PARAMETERS d3dpp;
-	ZeroMemory(&d3dpp, sizeof(d3dpp)); // xoa mọi thư vê 0 trươc khi sư dung
-
-	d3dpp.Windowed = TRUE;		// thê hiên ơ chê đô cửa sổ
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8; //>???
-		//-----------------đủ để khơi tao một cửa sổ để co thể vẽ bằng Direct3D.
-	d3dpp.BackBufferCount = 1;
-	d3dpp.BackBufferWidth = SCREEN_WIDTH;
-	d3dpp.BackBufferHeight = SCREEN_HEIGHT;
-	d3dpp.hDeviceWindow = hwnd;
-
-
-	d3d->CreateDevice(
-		D3DADAPTER_DEFAULT, // dùng card man hinh măc định
-		D3DDEVTYPE_HAL, // vẽ bằng phân cưng
-		hwnd,			//i.e. bằng card man hinh thay vi gia lập
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-		&d3dpp, // cac tham sô thê hiên cua thiêt bị
-		&d3ddev); // đôi tượng dev được tao ra
-
-
-	//set random number seed
-	srand(time(NULL));
-
-	//clear the backbuffer to black
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
-
-	//create surface
-	result = d3ddev->CreateOffscreenPlainSurface(
-		520, //width of the surface
-		720, //height of the surface
-		D3DFMT_X8R8G8B8, //surface format
-		D3DPOOL_DEFAULT, //memory pool to use
-		&surface, //pointer to the surface
-		NULL); //reserved (always NULL)
-
-	if (result != D3D_OK)
-		return 1;
-
-	//load surface from file into newly created surface
-	result = D3DXLoadSurfaceFromFile(
-		surface, //destination surface
-		NULL, //destination palette
-		NULL, //destination rectangle
-		"legotron.jpg", //source filename
-		NULL, //source rectangle
-		D3DX_DEFAULT, //controls how image is filtered
-		0, //for transparency (0 for none)
-		NULL); //source image info (usually NULL)
-
-	//make sure file was loaded okay
-	if (result != D3D_OK)
-		return 1;
-
-	//return okay
-	return 1;
-}
-
-//--------------------------------------------------------------------
-
-void Game_Run(HWND hwnd)
-{
-	RECT rect;
-	int r, g, b;
-
-	//đam bao con trỏ đên Direct3D la hợp lê
-	if (d3ddev == NULL)
-		return;
-
-	//đanh dâu băt đâu vẽ môt frame
-	if (d3ddev->BeginScene())
-	{
-		//create pointer to the back buffer
-		d3ddev->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backbuffer);
-		//draw surface to the backbuffer
-
-		d3ddev->StretchRect(surface, NULL, backbuffer, NULL, D3DTEXF_NONE);
-		//stop rendering
-		d3ddev->EndScene();
-	}
-
-
-	//display the back buffer on the screen
-	d3ddev->Present(NULL, NULL, NULL, NULL);
-
-	//check for escape key (to exit program)
-	if (KEY_DOWN(VK_ESCAPE))
-		PostMessage(hwnd, WM_DESTROY, 0, 0); //thoát game//thoát cd toàn màn hình
-}
-
-//------------------------------------------------------------------------------------
-
-void Game_End(HWND hwnd)
-{
-	MessageBox(hwnd, "Program is about to end", "Game_End", MB_OK);
-	//free the surface
-	surface->Release();
-
-	//release the Direct3D device
-	if (d3ddev != NULL) d3ddev->Release();
-	//release the Direct3D object
-	if (d3d != NULL) d3d->Release();
-}
-
-//-------------------------------------------------------------------------
-
+//entry point for a Windows program
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
-
-	MSG msg; // khai bao biên chưa thông điêp
-
-
-	////// đăng ky lơp cưa sô
-
-	MyRegisterClass(hInstance);
-
-
+	MSG msg;
 	HWND hWnd;
-
-
-
-	//khơi đông bô tao sô ngâu nhiên
-	srand(time(NULL));
-#if 0
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	} //trang 98
-	return msg.wParam;
-#endif
-	//tao môt cưa sô
+	// register the class
+	MyRegisterClass(hInstance);
+	//set up the screen in windowed or fullscreen mode?
+	DWORD style;
+	if (FULLSCREEN)
+		style = WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP;
+	else
+		style = WS_OVERLAPPED;
+	//create a new window
 	hWnd = CreateWindow(
 		APPTITLE, //window class
 		APPTITLE, //title bar
-		WS_OVERLAPPEDWINDOW, //tạo cửa sổ đóng mở dc (có -,x)
-		//WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP, //window style
+		style, //window style
 		CW_USEDEFAULT, //x position of window
 		CW_USEDEFAULT, //y position of window
 		SCREEN_WIDTH, //width of the window
@@ -240,41 +73,36 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		NULL, //menu
 		hInstance, //application instance
 		NULL); //window parameters
-
-	//kiêm tra lôi nêu không tao được cưa sô
+		//was there an error creating the window?
 	if (!hWnd)
 		return FALSE;
-
-	//hiên thị cưa sô
+	//display the window
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
-
-	//khơi tao game
-	if (!Game_Init(hWnd))
+	if (!Init_Direct3D(hWnd, SCREEN_WIDTH, SCREEN_HEIGHT,FULLSCREEN))
 		return 0;
-
-	// vong lăp thông điêp chinh
+	//initialize the game
+	if (!Game_Init(hWnd))
+	{
+		MessageBox(hWnd, "Error initializing the game","Error", MB_OK);
+		return 0;
+	}
+	// main message loop
 	int done = 0;
 	while (!done)
 	{
-
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
-
-			// kiêm tra xem co phai thông điêp thoat?
-
+			//look for quit message
 			if (msg.message == WM_QUIT)
 				done = 1;
-
-			//giai ma va truyên thông điêp cho WndProc
+			//decode and pass messages on to WndProc
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		//process game loop
 		else
-			//xư ly game
+			//process game loop (else prevents running after window is closed)
 			Game_Run(hWnd);
-	}//trang 99-100
+	}
 	return msg.wParam;
 }
-
---
+++ Game_Window/Load_sprite/game.cpp
@@ -0,0 +1,97 @@
+// Anim_Sprite program source code file
+#include "game.h"
+LPDIRECT3DSURFACE9 kitty_image[7];
+SPRITE kitty;
+//timing variable
+long start = GetTickCount();
+//initializes the game
+int Game_Init(HWND hwnd)
+{
+	char s[20];
+	int n;
+	//set random number seed
+	srand(time(NULL));
+	//load the sprite animation
+	for (n = 0; n < 6; n++)
+	{
+		sprintf(s, "cat%d.bmp", n + 1);
+		kitty_image[n] = LoadSurface(s, D3DCOLOR_XRGB(255, 0, 255));
+		if (kitty_image[n] == NULL)
+			return 0;
+	}
+	//initialize the sprite's properties
+	kitty.x = 100;
+	kitty.y = 150;
+	kitty.width = 96;
+	kitty.height = 96;
+	kitty.curframe = 0;
+	kitty.lastframe = 5;
+	kitty.animdelay = 2;
+	kitty.animcount = 0;
+	kitty.movex = 8;
+	kitty.movey = 0;
+	//return okay
+	return 1;
+}
+//the main game loop
+void Game_Run(HWND hwnd)
+{
+	RECT rect;
+	//make sure the Direct3D device is valid
+	if (d3ddev == NULL)
+		return;
+	//after short delay, ready for next frame?
+	//this keeps the game running at a steady frame rate
+	if (GetTickCount() - start >= 30)
+	{
+		//reset timing
+		start = GetTickCount();
+		//move the sprite
+		kitty.x += kitty.movex;
+		kitty.y += kitty.movey;
+		//"warp" the sprite at screen edges
+		if (kitty.x > SCREEN_WIDTH - kitty.width)
+			kitty.x = 0;
+		if (kitty.x < 0)
+			kitty.x = SCREEN_WIDTH - kitty.width;
+		//has animation delay reached threshold?
+		if (++kitty.animcount > kitty.animdelay)
+		{
+			//reset counter
+			kitty.animcount = 0;
+			//animate the sprite
+			if (++kitty.curframe > kitty.lastframe)
+				kitty.curframe = 0;
+		}
+	}
+	//start rendering
+	if (d3ddev->BeginScene())
+	{
+		//erase the entire background
+		d3ddev->ColorFill(backbuffer, NULL,
+			D3DCOLOR_XRGB(0, 0, 0));
+		//set the sprite's rect for drawing
+		rect.left = kitty.x;
+		rect.top = kitty.y;
+		rect.right = kitty.x + kitty.width;
+		rect.bottom = kitty.y + kitty.height;
+		//draw the sprite
+		d3ddev->StretchRect(kitty_image[kitty.curframe],
+			NULL, backbuffer, &rect, D3DTEXF_NONE);
+		//stop rendering
+		d3ddev->EndScene();
+	}
+	//display the back buffer on the screen
+	d3ddev->Present(NULL, NULL, NULL, NULL);
+	//check for escape key (to exit program)
+	if (KEY_DOWN(VK_ESCAPE))
+		PostMessage(hwnd, WM_DESTROY, 0, 0);
+}
+//frees memory and cleans up before the game ends
+void Game_End(HWND hwnd)
+{
+	int n;
+	//free the surface
+	for (n = 0; n < 6; n++)
+		kitty_image[n]->Release();
+}
